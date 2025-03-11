4c4cfe4ad24573a645d9ebc6ecf826ef38eaaa93
gr422_hadal <denishatkov17@gmail.com>
2025-03-11 17:45:29 +0700

    2 Commit

--- js/main.js
+++ js/main.js
@@ -1,34 +1,80 @@
-let app = new Vue ({
-    el: '#app',
-    data: {
-        product: "Socks",
-        image: "./assets/vmSocks-green-onWhite.jpg",
-        altText: "A pair of socks",
-        inStock: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 2234,
-                variantColor: 'green',
-                variantImage: "./assets/vmSocks-green-onWhite.jpg",
-            },
-            {
-                variantId: 2235,
-                variantColor: 'blue',
-                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
-            }
-         ],         
-         cart: 0,
+Vue.component('product', {
+    template: `
+    <div class="product">
+        <div class="product-image">
+            <img :src="image" :alt="altText"/>
+        </div>
+
+        <div class="product-info">
+            <h1>{{ title }}</h1>
+            <p v-if="inStock">In stock</p>
+            <p :class="{ lineThrough: !inStock }" v-else>Out of Stock</p>
+            <ul>
+                <li v-for="detail in details">{{ detail }}</li>
+            </ul>
+
+            <div class="color-box" v-for="(variant, index) in variants" :key="variant.variantId"
+                 :style="{ backgroundColor:variant.variantColor }" @mouseover="updateProduct(index)">
+            </div>
+
+            <div class="cart">
+                <p>Cart({{ cart }})</p>
+            </div>
+            <p v-show="onSale">{{sale}}</p>
+
+            <button v-on:click="addToCart" :disabled="!inStock" :class="{ disabledButton: !inStock }">Add to cart</button>
+        </div>
+    </div>
+    `,
+    data() {
+        return {
+            product: "Socks",
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
+            onSale: true,
+            altText: "A pair of socks",
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 2234,
+                    variantColor: 'green',
+                    variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                    variantQuantity: 10,
+                },
+                {
+                    variantId: 2235,
+                    variantColor: 'blue',
+                    variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                    variantQuantity: 0,
+                }
+            ],
+            cart: 0,
+        }
     },
     methods: {
         addToCart() {
             this.cart += 1;
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+            console.log(index);
         },
-        removeToCart() {
-            this.cart -= 1;
-        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
+        sale() {
+            return this.brand + ' ' + this.product + ' IS SALE!!!!';
+        },
     }
+})
+let app = new Vue({
+    el: '#app',
 })
